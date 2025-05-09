bdd111d7c5a9bd35db16b47f4ec2179f45c3ffab
CamilliFeliz <camilliffeliz@gmail.com>
2025-05-09 19:46:05 -0300

    commit push 1.0

---
+++ Models/Produto.cs
@@ -0,0 +1,14 @@
+namespace Banco.Models  {
+    using System;
+
+    public class Produto {
+        public string[] nome =  new string[5];
+        public double preco;
+
+        public void exibirProduto(){
+
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Using.System;
+
+
